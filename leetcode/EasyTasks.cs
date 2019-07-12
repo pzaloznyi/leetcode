e002c834ae0020f0b4276318698c01105d67d88f
Pavlo Zaloznyi <pzaloznyi@gmail.com>
2019-07-12 17:00:33 +0300

    convert roman to integer

--- leetcode/EasyTasks.cs
+++ leetcode/EasyTasks.cs
@@ -1,3 +1,4 @@
+using System;
 using FluentAssertions;
 using Xunit;
 using Xunit.Abstractions;
@@ -31,6 +32,17 @@ namespace leetcode
         public void IsPalindrome(int actual, bool expected)
         {
             _solution.IsPalindrome(actual).Should().Be(expected);
+        }
+
+        [Theory(DisplayName = "https://leetcode.com/problems/roman-to-integer/")]
+        [InlineData("III", 3)]
+        [InlineData("IV", 4)]
+        [InlineData("IX", 9)]
+        [InlineData("LVIII", 58)]
+        [InlineData("MCMXCIV", 1994)]
+        public void RomanToInt(string actual, int expected)
+        {
+            _solution.RomanToInt(actual).Should().Be(expected);
         }
     }
 }
--- leetcode/Solution.cs
+++ leetcode/Solution.cs
@@ -28,7 +28,59 @@ namespace leetcode
         
         public bool IsPalindrome(int x)
         {
-            return x >= 0 && x == Reverse(x);
+            if (x < 0 || (x % 10 == 0 && x != 0))
+            {
+                return false;
+            }
+
+            int revertedNumber = 0;
+            while (x > revertedNumber)
+            {
+                revertedNumber = revertedNumber * 10 + x % 10;
+                x /= 10;
+            }
+
+            return x == revertedNumber || x == revertedNumber / 10;
+        }
+        
+        public int RomanToInt(string number)
+        {
+            var result = 0;
+            var index = 0;
+            var previous = 0;
+            while (index < number.Length)
+            {
+                var current = RomanToDigit(number[index]);
+
+                if (current > previous)
+                {
+                    result += current - 2 * previous;
+                }
+                else
+                {
+                    result += current;
+                }
+
+                previous = current;
+                index++;
+            }
+
+            return result;
+        }
+        
+        private static int RomanToDigit(char digit)
+        {
+            switch (digit)
+            {
+                case 'I': return 1;
+                case 'V': return 5;
+                case 'X': return 10;
+                case 'L': return 50;
+                case 'C': return 100;
+                case 'D': return 500;
+                case 'M': return 1000;
+                default: return 0;
+            }
         }
     }
 }
